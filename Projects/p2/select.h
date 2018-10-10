b68a634113c7ee123f79ac64f2985ea54ef9a161
charleschang213 <charleschang213@sjtu.edu.cn>
2018-10-11 03:43:18 +0800

    20181010

---
+++ Projects/p2/select.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: select.h
+    > Author: Charles Chang
+    > Mail: charleschang213@sjtu.edu.cn 
+    > Created Time: 2018-10-11 03:40:31
+ ************************************************************************/
+
+#ifdef SELECT_H
+#define SELECT_H
+int rselect(int *arr,int size,int k);
+int dselect(int *arr,int size,int k);
+#endif
