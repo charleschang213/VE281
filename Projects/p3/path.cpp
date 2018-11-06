6341cba28fd4243c4d292c3aa47f04bf41fb2bb8
charleschang213 <869048286@qq.com>
2018-11-06 09:30:08 +0800

    dasda

--- Projects/p3/path.cpp
+++ Projects/p3/path.cpp
@@ -31,10 +31,8 @@ void path(long int m,long int n, point_t **map,long int sx,long int sy,long int 
             map[y1][x1].pre = &(map[c.y][c.x]);
             if ((x1==ex)&&(y1==ey)){
                 if (v) std::cout <<  "Cell (" << x1 << ", " << y1 << ") with accumulated length " << map[y1][x1].pathcost << " is the ending point.\n";
-#ifndef P3_CLOCK
 				std::cout << "The shortest path from (" << sx << ", " << sy << ") to (" << ex << ", " << ey << ") is " << map[y1][x1].pathcost << ".\nPath:\n";
 				backtrace(map, ex,ey);
-#endif
                 delete pq;
                 return; 
             } 
