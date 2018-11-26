8f516fda2a5777460c82f3195326cc76b541c7b4
charleschang213 <869048286@qq.com>
2018-11-26 11:34:30 +0800

    123

--- Projects/p4/orderbook.cpp
+++ Projects/p4/orderbook.cpp
@@ -43,74 +43,90 @@ void orderbook::order_execute(order neworder){
     equity &eq = this->equities[neworder.geteid()];
     client &c1 = this->clients[neworder.getcid()];
     if (neworder.getbuy()){
-        if (eq.sellercount()==0){
-            if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
-                eq.addbuyer(neworder);
-            return;
-        }
-        order seller = eq.getseller();
-        while (seller.getduration()!=-1&&seller.getduration()<=this->timestamp) {
+        while (neworder.getshare()!=0){
             if (eq.sellercount()==0){
                 if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
                     eq.addbuyer(neworder);
                 return;
             }
-            seller = eq.getseller();
+            order seller = eq.getseller();
+            while (seller.getduration()!=-1&&seller.getduration()<=this->timestamp) {
+                if (eq.sellercount()==0){
+                    if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
+                        eq.addbuyer(neworder);
+                    return;
+                }
+                seller = eq.getseller();
+            }
+            client &c2 = this->clients[seller.getcid()];
+            if (seller.getprice()<=neworder.getprice()){
+                int dshare = MIN(seller.getshare(),neworder.getshare());
+                seller.doneshare(dshare);
+                neworder.doneshare(dshare);
+                int commision = seller.getprice()*dshare/100;
+                this->income+=2*commision;
+                c2.sell(dshare,seller.getprice()*dshare);
+                c1.buy(dshare,seller.getprice()*dshare);
+                this->transferred+=seller.getprice()*dshare;
+                this->completed++;
+                this->cshare+=dshare;
+                if (this->verbose)
+                    std::cout << c1.getname() << " purchased " << dshare << " shares of " << eq.getname() << " from " << c2.getname() << " for $" << seller.getprice() << "/share" << std::endl; 
+            }
+            else{
+                eq.addseller(seller);
+                if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
+                        eq.addbuyer(neworder);
+                    return;
+            }
+            if ((neworder.getduration()==-1||neworder.getduration()>=this->timestamp)&&(neworder.getshare()!=0))
+                eq.addbuyer(neworder);
+            if ((seller.getduration()==-1||seller.getduration()>=this->timestamp)&&(seller.getshare()!=0))
+                eq.addseller(seller);
         }
-        client &c2 = this->clients[seller.getcid()];
-        if (seller.getprice()<=neworder.getprice()){
-            int dshare = MIN(seller.getshare(),neworder.getshare());
-            seller.doneshare(dshare);
-            neworder.doneshare(dshare);
-            int commision = seller.getprice()*dshare/100;
-            this->income+=2*commision;
-            c2.sell(dshare,seller.getprice()*dshare);
-            c1.buy(dshare,seller.getprice()*dshare);
-            this->transferred+=seller.getprice()*dshare;
-            this->completed++;
-            this->cshare+=dshare;
-            if (this->verbose)
-                std::cout << c1.getname() << " purchased " << dshare << " shares of " << eq.getname() << " from " << c2.getname() << " for $" << seller.getprice() << "/share" << std::endl; 
-        }
-        if ((neworder.getduration()==-1||neworder.getduration()>=this->timestamp)&&(neworder.getshare()!=0))
-            eq.addbuyer(neworder);
-        if ((seller.getduration()==-1||seller.getduration()>=this->timestamp)&&(seller.getshare()!=0))
-            eq.addseller(seller);
     }
     else {
-        if (eq.buyercount()==0){
-            if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
-                eq.addseller(neworder);
-            return;
-        }
-        order buyer = eq.getbuyer();
-        while (buyer.getduration()!=-1&&buyer.getduration()<=this->timestamp) {
-            if (eq.sellercount()==0){
+        while (neworder.getshare()!=0){
+            if (eq.buyercount()==0){
                 if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
                     eq.addseller(neworder);
                 return;
             }
-            buyer = eq.getbuyer();
+            order buyer = eq.getbuyer();
+            while (buyer.getduration()!=-1&&buyer.getduration()<=this->timestamp) {
+                if (eq.sellercount()==0){
+                    if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
+                        eq.addseller(neworder);
+                    return;
+                }
+                buyer = eq.getbuyer();
+            }
+            client &c2 = this->clients[buyer.getcid()];
+            if (neworder.getprice()<=buyer.getprice()){
+                int dshare = MIN(buyer.getshare(),neworder.getshare());
+                buyer.doneshare(dshare);
+                neworder.doneshare(dshare);
+                int commision = buyer.getprice()*dshare/100;
+                this->income+=2*commision;
+                c2.buy(dshare,buyer.getprice()*dshare);
+                c1.sell(dshare,buyer.getprice()*dshare);
+                this->transferred+=buyer.getprice()*dshare;
+                this->completed++;
+                this->cshare+=dshare;
+                if (this->verbose)
+                    std::cout << c2.getname() << " purchased " << dshare << " shares of " << eq.getname() << " from " << c1.getname() << " for $" << buyer.getprice() << "/share" << std::endl; 
+            }
+            else {
+                eq.addbuyer(buyer);
+                if (neworder.getduration()==-1||neworder.getduration()>this->timestamp)
+                    eq.addseller(neworder);
+                return;
+            }
+            if ((neworder.getduration()==-1||neworder.getduration()>this->timestamp)&&(neworder.getshare()!=0))
+                eq.addseller(neworder);
+            if ((buyer.getduration()==-1||buyer.getduration()>this->timestamp)&&(buyer.getshare()!=0))
+                eq.addbuyer(buyer);
         }
-        client &c2 = this->clients[buyer.getcid()];
-        if (neworder.getprice()<=buyer.getprice()){
-            int dshare = MIN(buyer.getshare(),neworder.getshare());
-            buyer.doneshare(dshare);
-            neworder.doneshare(dshare);
-            int commision = buyer.getprice()*dshare/100;
-            this->income+=2*commision;
-            c2.buy(dshare,buyer.getprice()*dshare);
-            c1.sell(dshare,buyer.getprice()*dshare);
-            this->transferred+=buyer.getprice()*dshare;
-            this->completed++;
-            this->cshare+=dshare;
-            if (this->verbose)
-                std::cout << c2.getname() << " purchased " << dshare << " shares of " << eq.getname() << " from " << c1.getname() << " for $" << buyer.getprice() << "/share" << std::endl; 
-        }
-        if ((neworder.getduration()==-1||neworder.getduration()>this->timestamp)&&(neworder.getshare()!=0))
-            eq.addseller(neworder);
-        if ((buyer.getduration()==-1||buyer.getduration()>this->timestamp)&&(buyer.getshare()!=0))
-            eq.addbuyer(buyer);
     }
 }
 
